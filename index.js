3916b478aa6958be2cf648e601015660f8aa830a
MaryMachuma <machhmary173@gmail.com>
2024-12-04 07:15:40 +0300

    Adds Functions continued

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Function  mondayWork
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Function wrapAdjective
+  function wrapAdjective(wrapper = "*") {
+    return function(adjective) {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+  }
+  
+  // Exporting functions if needed (optional for Node.js environments)
+  module.exports = { saturdayFun, mondayWork, wrapAdjective };
+  
